aed7c0ef99e0d986fa66f91fb7ed927a6dc548dd
Yarden Mathieson <yardenm2710@gmail.com>
2022-03-28 16:51:32 +0300

    added mobile design

--- script/script.js
+++ script/script.js
@@ -1,7 +1,36 @@
 var isOpen = false;
 var navbar = document.querySelector('.navbar-section');
+var deviderX = document.querySelectorAll('.card-devider-x');
+var deviderY = document.querySelectorAll('.card-devider-y');
+
+function onInit() {
+  createDevidersX();
+  createDevidersY();
+}
 
 function toggleNavBar() {
   isOpen ? navbar.classList.remove('open') : navbar.classList.add('open');
   isOpen = !isOpen;
+}
+
+function createDevidersX() {
+  var strHtml = '';
+  for (var i = 0; i < 30; i++) {
+    strHtml += '<img src="./images/seperator-x.png">';
+  }
+
+  deviderX.forEach((element) => {
+    element.innerHTML = strHtml;
+  });
+}
+
+function createDevidersY() {
+  var strHtml = '';
+  for (var i = 0; i < 35; i++) {
+    strHtml += '<img src="./images/seperator-y.png">';
+  }
+
+  deviderY.forEach((element) => {
+    element.innerHTML = strHtml;
+  });
 }
